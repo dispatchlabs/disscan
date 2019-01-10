31e4ce7e3e9bf04999c54933b6c6ea9297bdaf63
Brian Woods <me@brian.biz>
2019-01-10 11:34:41 -0800

    Removig unnecessary things from hashes

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -272,9 +272,7 @@ export class AppService extends M2Service implements OnDestroy {
         const from = Buffer.from(transaction.from, 'hex');
         const to = Buffer.from(transaction.to, 'hex');
         const value = this.stringToBuffer(transaction.value);
-        const params = this.stringToBuffer(transaction.params || '');
         const time = this.numberToBuffer(transaction.time);
-        const code = Buffer.from(transaction.code || '', 'hex');
 
         // Type?
         switch (transaction.type) {
@@ -282,11 +280,13 @@ export class AppService extends M2Service implements OnDestroy {
                 hash = keccak('keccak256').update(Buffer.concat([Buffer.from('00', 'hex'), from, to, value, time])).digest();
                 break;
             case TransactionType.DeploySmartContract:
+                const code = Buffer.from(transaction.code, 'hex');
                 hash = keccak('keccak256').update(Buffer.concat([Buffer.from('01', 'hex'), from, to, value, code, time])).digest();
                 break;
             case TransactionType.ExecuteSmartContract:
+                const params = this.stringToBuffer(transaction.params);
                 const method = this.stringToBuffer(transaction.method);
-                hash = keccak('keccak256').update(Buffer.concat([Buffer.from('02', 'hex'), from, to, value, code, method, params, time])).digest();
+                hash = keccak('keccak256').update(Buffer.concat([Buffer.from('02', 'hex'), from, to, value, method, params, time])).digest();
                 break;
         }
         transaction.hash = hash.toString('hex');
