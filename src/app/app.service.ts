77d1f12842107b92c143edb77ee2aeb8c2651c07
Zane Witherspoon <ZaneWithSpoon@users.noreply.github.com>
2019-01-15 12:27:28 -0800

    merging dev into dg-1163 to include params update (#36)
    * DG 1109 Fixes contract execution from scanner (#31)
    
    * Updated Readme for new logo and name
    
    * Update Readme logo size
    
    * DG-1109 fixes exe contract, stringify's params
    
    * fixes issue with params
    
    * Removig unnecessary things from hashes
    
    * DG 1109 Fixes contract execution from scanner (#31) (#34) (#35)
    
    * Updated Readme for new logo and name
    
    * Update Readme logo size
    
    * DG-1109 fixes exe contract, stringify's params
    
    * fixes issue with params
    
    * Removig unnecessary things from hashes

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -292,7 +292,6 @@ export class AppService extends M2Service implements OnDestroy {
         const value = this.stringToBuffer(transaction.value);
         const params = this.stringToBuffer(transaction.params || '');
         const time = this.numberToBuffer(transaction.time);
-        const code = Buffer.from(transaction.code || '', 'hex');
 
         // Type?
         switch (transaction.type) {
@@ -303,8 +302,9 @@ export class AppService extends M2Service implements OnDestroy {
                 hash = keccak('keccak256').update(Buffer.concat([Buffer.from('01', 'hex'), from, to, value, code, time])).digest();
                 break;
             case TransactionType.ExecuteSmartContract:
+                const params = this.stringToBuffer(transaction.params);
                 const method = this.stringToBuffer(transaction.method);
-                hash = keccak('keccak256').update(Buffer.concat([Buffer.from('02', 'hex'), from, to, value, code, method, params, time])).digest();
+                hash = keccak('keccak256').update(Buffer.concat([Buffer.from('02', 'hex'), from, to, value, method, params, time])).digest();
                 break;
         }
         transaction.hash = hash.toString('hex');
